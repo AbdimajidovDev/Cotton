579445950dab305a0e7aa4a283ca4a21d6136000
azimjnovislom77 <azimjonovislomjon77@gmail.com>
2025-09-10 15:01:42 +0500

    region api done

--- app/region/models.py
+++ app/region/models.py
@@ -1,59 +1,58 @@
 from django.db import models
-
-from app.users.models import User
+from django.conf import settings
 
 
 class Region(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
     name = models.CharField(max_length=255)
     plan = models.FloatField()
     crop_area = models.FloatField()
     farm = models.ForeignKey("Farm", on_delete=models.CASCADE)
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
 
 class District(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
     region = models.ForeignKey(Region, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     plan = models.FloatField()
     crop_area = models.FloatField()
     farm = models.ForeignKey("Farm", on_delete=models.CASCADE)
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
 
 class Massive(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
     district = models.ForeignKey(District, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     plan = models.FloatField()
     crop_area = models.FloatField()
     farm = models.ForeignKey("Farm", on_delete=models.CASCADE)
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
 
-class Neighborhood(models.Model):  # mahalla
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+class Neighborhood(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
     massive = models.ForeignKey(Massive, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     crop_area = models.FloatField()
     farm = models.ForeignKey("Farm", on_delete=models.CASCADE)
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
 
-class Farm(models.Model):  # farm
+class Farm(models.Model):
     full_name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
     INN = models.CharField(max_length=20, unique=True)
@@ -61,4 +60,3 @@ class Farm(models.Model):  # farm
 
     def __str__(self):
         return self.full_name
-
--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('users/', include('app.users.urls')),
-    # path('region/', include('app.region.urls')),
+    path('region/', include('app.region.urls')),
     # path('squad/', include('app.squad.urls')),
 ]
