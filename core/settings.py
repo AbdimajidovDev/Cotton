5155f82c1bada5e4ed2e4c7c6d61cbc315b594eb
To'lqinjon <tulqinjonabdimajidov@gmail.com>
2025-09-13 16:24:46 +0500

    fixed

--- core/settings.py
+++ core/settings.py
@@ -156,6 +156,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
